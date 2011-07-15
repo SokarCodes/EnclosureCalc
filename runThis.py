c2d73f855aae8c6a3cbf328baab354a8c5576836
Jukka Vatjus-Anttila <jukka.vatjus-anttila@cie.fi>
2011-07-15 09:40:29 +0300

    Added runThis.py which runs testdrive for class enclosureManager

---
+++ runThis.py
@@ -0,0 +1,21 @@
+from enclosureCalc import enclosureManager
+
+# Test program starts here
+print '''************************************************
+Welcome to test program. This program testdrives enclosureManager class by setting parameters.
+You can modify it yourself to test different scenarios although there are not much to test yet.
+************************************************'''
+newProject = enclosureManager("testi")
+enclosureManager.printProjects()
+newProject.setDimensions(51.0,62.0,61.0,2.2)
+print 'Setting port: 15.24 cm diameter and 20Hz tune' 
+newProject.setPort(15.24, 20)
+newProject.printAll()
+print 'setting driver'
+newProject.setDriver(5)
+newProject.printAll()
+print 'Setting height: 65cm'
+newProject.setHeight(65.0)
+newProject.printAll()
+del newProject
+# program ends here
